1e4623ddb8503c89c014d7723642d79d3c639eca
Arthur Dooner <shunderpooch@gmail.com>
2016-04-28 03:03:04 -0400

    Started working on board class in Kabasuji

--- DolonKabasuji/src/KabasujiModel/Board.java
+++ DolonKabasuji/src/KabasujiModel/Board.java
@@ -4,19 +4,21 @@ import java.util.ArrayList;
  * Created by Arthur on 4/10/2016.
  */
 public class Board {
-    ArrayList<Tile> tiles;
+    ArrayList<ArrayList<Tile>> tiles; //Rows holding columns
     ArrayList<Piece> piecesOnBoard;
     int numColumns, numRows;
 
     public Board() {
         //Initialize tiles
         piecesOnBoard = new ArrayList<Piece>();
+        numColumns = 0;
+        numRows = 0;
     }
     //Takes in a piece to add to the board
     //Adds a piece to the board
     //Returns true if successfully added to the board, false otherwise
     public boolean addPiece(Piece p, int xLoc, int yLoc){
-      //  Array
+
         return false;
     }
 
@@ -27,8 +29,29 @@ public class Board {
         return false;
     }
 
+    //Sets the number of columns for the board
     public void setNumColumns(int numColumns){
-        this.numColumns = numColumns;
+        int oldColumns = this.numColumns; //Number of columns that we currently have
+        this.numColumns = numColumns; //Number of columns that we want
+        if (numColumns < 1){ //We shouldn't do this, guys. Come on.
+            throw(new IndexOutOfBoundsException("Numcolumns should not be less than 1"));
+        }
+        //Iterate down the rows
+        for (int y = 0; y < numRows; y++){
+            ArrayList<Tile> tempArrayList = tiles.get(y); //Get the temporary arraylist
+
+            if (numColumns > oldColumns){ //If the number of columns we want is larger
+                for (int x = 0; x < (numColumns - oldColumns); x++){ //Add the difference
+                    tempArrayList.add(new Tile()); //Creates a new, valid tile in our grid
+                }
+            }
+            else if (numColumns < oldColumns){ //If we want fewer columns
+                for (int x = 0; x < (oldColumns - numColumns); x++){ //Remove the difference
+                    tempArrayList.remove(tempArrayList.size() - 1); //Remove the last element on the array as many times as possible
+                }
+            }
+            tiles.set(y, tempArrayList); //Sets the new tiles to have this arraylist info
+        }
     }
 
     public void setNumRows(int numColumns){
